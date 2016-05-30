3b4a21c2b85d347c9830c5eb2c09f46e09d4b7f4
Gerardo Teruel <gerardogtn@gmail.com>
2016-05-30 13:42:50 -0500

    :rocket: Add WSGI support

--- hello.py
+++ hello.py
@@ -14,10 +14,13 @@ settings.configure(
 )
 
 from django.conf.urls import url
+from django.core.wsgi import get_wsgi_application
 from django.http import HttpResponse
 
 def index(request):
     return HttpResponse('Hello world!')
+
+application = get_wsgi_application()
 
 urlpatterns = (
     url(r'^$', index),
